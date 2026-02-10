1bfb742aa38198bc1d748736e311f1e156b9e99a
Meenakshi_Ganesh <meenakshiganesh1111@gmail.com>
2026-02-10 12:30:32 +0530

    Add Welcome to Java solution

---
+++ Introduction/WelcomeToJava.java
@@ -0,0 +1,10 @@
+// Problem: Welcome to Java
+// Platform: HackerRank
+// Difficulty: Easy
+
+public class WelcomeToJava {
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
