a5191698b0bae72e74e6e9832e666de1410de6bb
Meenakshi_Ganesh <meenakshiganesh1111@gmail.com>
2026-02-11 17:07:31 +0530

    Add Java Output Formatting solution
    HackerRank Java Introduction problem using printf formatting

---
+++ Introduction/Java_Output_Formatting.java
@@ -0,0 +1,23 @@
+// HackerRank - Java Output Formatting
+// Topic: printf, formatted output
+// Difficulty: Easy
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("================================");
+
+        for (int i = 0; i < 3; i++) {
+            String s = sc.next();
+            int x = sc.nextInt();
+
+            System.out.printf("%-15s%03d%n", s, x);
+        }
+
+        System.out.println("================================");
+
+        sc.close();
+    }
+}
